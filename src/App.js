05bbb12cea94991dd3508d04ad57b3fc9191a990
Gabriel Mania <mania.gabb@gmail.com>
2023-03-15 18:58:51 +0800

    replaced API keys with environment variables

--- src/App.js
+++ src/App.js
@@ -1,160 +1,237 @@
-import { useState, useEffect } from 'react';
-import { NFTStorage, File } from 'nft.storage'
-import { Buffer } from 'buffer';
-import { ethers } from 'ethers';
-import axios from 'axios';
+import { useState, useEffect, useRef } from "react";
+import { NFTStorage, File } from "nft.storage";
+import { Buffer } from "buffer";
+import { ethers, providers } from "ethers";
+import axios from "axios";
+import Web3Modal from "web3modal";
 
 // Components
-import Spinner from 'react-bootstrap/Spinner';
-import Navigation from './components/Navigation';
+import Spinner from "react-bootstrap/Spinner";
+import Navigation from "./components/Navigation";
 
 // ABIs
-import NFT from './abis/NFT.json'
+import NFT_ABI from "./abis/NFT.json";
 
-// Config
-import config from './config.json';
+/* // Config
+import config from './config.json'; */
+
+const NFT_CONTRACT_ADDRESS = "0x41002cE425978937DaFE3dB0465dB8D6C392E965";
+/* - Goerli - 0x41002cE425978937DaFE3dB0465dB8D6C392E965
+- Sepolia - 0xc40cD860a5af0383Bd61a52eC1094cF9A9227aA3 */
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
-  const [nft, setNFT] = useState(null)
+    const [provider, setProvider] = useState(null);
+    const [account, setAccount] = useState(null);
+    const [nft, setNFT] = useState(null);
 
-  const [name, setName] = useState("")
-  const [description, setDescription] = useState("")
-  const [image, setImage] = useState(null)
-  const [url, setURL] = useState(null)
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState(null);
+    const [url, setURL] = useState(null);
 
-  const [message, setMessage] = useState("")
-  const [isWaiting, setIsWaiting] = useState(false)
+    const [message, setMessage] = useState("");
+    const [isWaiting, setIsWaiting] = useState(false);
 
-  const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
+    const [walletConnected, setWalletConnected] = useState(false);
+    const web3ModalRef = useRef();
 
-    const network = await provider.getNetwork()
+    const loadBlockchainData = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
 
-    const nft = new ethers.Contract(config[network.chainId].nft.address, NFT, provider)
-    setNFT(nft)
-  }
+        const nft = new ethers.Contract(
+            NFT_CONTRACT_ADDRESS,
+            NFT_ABI,
+            provider
+        );
+        setNFT(nft);
+    };
 
-  const submitHandler = async (e) => {
-    e.preventDefault()
+    const submitHandler = async (e) => {
+        e.preventDefault();
 
-    if (name === "" || description === "") {
-      window.alert("Please provide a name and description")
-      return
-    }
+        if (name === "" || description === "") {
+            window.alert("Please provide a name and description");
+            return;
+        }
 
-    setIsWaiting(true)
+        setIsWaiting(true);
 
-    // Call AI API to generate a image based on description
-    const imageData = await createImage()
+        // Call AI API to generate a image based on description
+        const imageData = await createImage();
 
-    // Upload image to IPFS (NFT.Storage)
-    const url = await uploadImage(imageData)
+        // Upload image to IPFS (NFT.Storage)
+        const url = await uploadImage(imageData);
 
-    // Mint NFT
-    await mintImage(url)
+        // Mint NFT
+        await mintImage(url);
 
-    setIsWaiting(false)
-    setMessage("")
-  }
+        setIsWaiting(false);
+        setMessage("");
+    };
 
-  const createImage = async () => {
-    setMessage("Generating Image...")
+    const createImage = async () => {
+        setMessage("Generating Image...");
 
-    // You can replace this with different model API's
-    const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`
+        // You can replace this with different model API's
+        const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`;
 
-    // Send the request
-    const response = await axios({
-      url: URL,
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      data: JSON.stringify({
-        inputs: description, options: { wait_for_model: true },
-      }),
-      responseType: 'arraybuffer',
-    })
+        // Send the request
+        const response = await axios({
+            url: URL,
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            data: JSON.stringify({
+                inputs: description,
+                options: { wait_for_model: true },
+            }),
+            responseType: "arraybuffer",
+        });
 
-    const type = response.headers['content-type']
-    const data = response.data
+        const type = response.headers["content-type"];
+        const data = response.data;
 
-    const base64data = Buffer.from(data).toString('base64')
-    const img = `data:${type};base64,` + base64data // <-- This is so we can render it on the page
-    setImage(img)
+        const base64data = Buffer.from(data).toString("base64");
+        const img = `data:${type};base64,` + base64data; // <-- This is so we can render it on the page
+        setImage(img);
 
-    return data
-  }
+        return data;
+    };
 
-  const uploadImage = async (imageData) => {
-    setMessage("Uploading Image...")
+    const uploadImage = async (imageData) => {
+        setMessage("Uploading Image...");
 
-    // Create instance to NFT.Storage
-    const nftstorage = new NFTStorage({ token: process.env.REACT_APP_NFT_STORAGE_API_KEY })
+        // Create instance to NFT.Storage
+        const nftstorage = new NFTStorage({
+            token: process.env.REACT_APP_NFT_STORAGE_API_KEY,
+        });
 
-    // Send request to store image
-    const { ipnft } = await nftstorage.store({
-      image: new File([imageData], "image.jpeg", { type: "image/jpeg" }),
-      name: name,
-      description: description,
-    })
+        // Send request to store image
+        const { ipnft } = await nftstorage.store({
+            image: new File([imageData], "image.jpeg", { type: "image/jpeg" }),
+            name: name,
+            description: description,
+        });
 
-    // Save the URL
-    const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`
-    setURL(url)
+        // Save the URL
+        const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`;
+        setURL(url);
 
-    return url
-  }
+        return url;
+    };
 
-  const mintImage = async (tokenURI) => {
-    setMessage("Waiting for Mint...")
+    const mintImage = async (tokenURI) => {
+        setMessage("Waiting for Mint...");
 
-    const signer = await provider.getSigner()
-    const transaction = await nft.connect(signer).mint(tokenURI, { value: ethers.utils.parseUnits("1", "ether") })
-    await transaction.wait()
-  }
+        const signer = await provider.getSigner();
+        const transaction = await nft.connect(signer).mint(tokenURI, {
+            value: ethers.utils.parseUnits("0.01", "ether"),
+        });
+        await transaction.wait();
+    };
 
-  useEffect(() => {
-    loadBlockchainData()
-  }, [])
+    const getProviderOrSigner = async (needSigner = false) => {
+        // Connect to Metamask
+        // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+        const provider = await web3ModalRef.current.connect();
+        const web3Provider = new providers.Web3Provider(provider);
 
-  return (
-    <div>
-      <Navigation account={account} setAccount={setAccount} />
+        // If user is not connected to the Goerli network, let them know and throw an error
+        /*  chain ID for networks:
+        Goerli - 5
+        Mumbai - 80001
+        Sepolia - 11155111 */
+        const { chainId } = await web3Provider.getNetwork();
+        if (chainId !== 5) {
+            window.alert("Change the network to Goerli");
+            throw new Error("Change network to Goerli");
+        }
 
-      <div className='form'>
-        <form onSubmit={submitHandler}>
-          <input type="text" placeholder="Create a name..." onChange={(e) => { setName(e.target.value) }} />
-          <input type="text" placeholder="Create a description..." onChange={(e) => setDescription(e.target.value)} />
-          <input type="submit" value="Create & Mint" />
-        </form>
+        if (needSigner) {
+            const signer = web3Provider.getSigner();
+            return signer;
+        }
+        return web3Provider;
+    };
 
-        <div className="image">
-          {!isWaiting && image ? (
-            <img src={image} alt="AI generated image" />
-          ) : isWaiting ? (
-            <div className="image__placeholder">
-              <Spinner animation="border" />
-              <p>{message}</p>
+    const connectWallet = async () => {
+        try {
+            // Get the provider from web3Modal, which in our case is MetaMask
+            // When used for the first time, it prompts the user to connect their wallet
+            await getProviderOrSigner();
+            setWalletConnected(true);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        loadBlockchainData();
+    }, []);
+
+    useEffect(() => {
+        // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+        if (!walletConnected) {
+            // Assign the Web3Modal class to the reference object by setting it's `current` value
+            // The `current` value is persisted throughout as long as this page is open
+            web3ModalRef.current = new Web3Modal({
+                network: "goerli",
+                providerOptions: {},
+                disableInjectedProvider: false,
+            });
+            connectWallet();
+        }
+    }, [walletConnected]);
+
+    return (
+        <div>
+            <Navigation account={account} setAccount={setAccount} />
+
+            <div className="form">
+                <form onSubmit={submitHandler}>
+                    <input
+                        type="text"
+                        placeholder="Create a name..."
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Create a description..."
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <input type="submit" value="Create & Mint" />
+                </form>
+
+                <div className="image">
+                    {!isWaiting && image ? (
+                        <img src={image} alt="AI generated image" />
+                    ) : isWaiting ? (
+                        <div className="image__placeholder">
+                            <Spinner animation="border" />
+                            <p>{message}</p>
+                        </div>
+                    ) : (
+                        <></>
+                    )}
+                </div>
             </div>
-          ) : (
-            <></>
-          )}
-        </div>
-      </div>
 
-      {!isWaiting && url && (
-        <p>
-          View&nbsp;<a href={url} target="_blank" rel="noreferrer">Metadata</a>
-        </p>
-      )}
-    </div>
-  );
+            {!isWaiting && url && (
+                <p>
+                    View&nbsp;
+                    <a href={url} target="_blank" rel="noreferrer">
+                        Metadata
+                    </a>
+                </p>
+            )}
+        </div>
+    );
 }
 
 export default App;
